8066fe5e09e901693e3a4e7757b69c86efe2cc17
DARWIN\Usuario <u201816660@upc.edu.pe>
2018-09-15 19:43:42 -0500

    Validations for Rents and Services

--- Parkeate_DW/app/models/rent.rb
+++ Parkeate_DW/app/models/rent.rb
@@ -1,2 +1,6 @@
 class Rent < ApplicationRecord
+
+	validates :id_usuario, :fecha_inicio, :fecha_fin, :id_estacionamiento, presence: {message: 'Obligatorio'}
+	validates :observaciones, length: {maximum:150 ,too_long:'Maximo %{count} caracteres'}
+    
 end
--- Parkeate_DW/app/models/service.rb
+++ Parkeate_DW/app/models/service.rb
@@ -1,2 +1,6 @@
 class Service < ApplicationRecord
+	validates :tipo_servicio, :descripcion,:tarifa, :id_estacionamiento, presence: {message: 'Obligatorio'}
+	validates :descripcion, length: {maximum:150 ,too_long:'Maximo %{count} caracteres'}
+    validates :tarifa, numericality:{greater_than:0, less_than:100, message:'incorrecto'}
+
 end
