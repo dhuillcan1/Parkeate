b2f103a22655cdebe81b9ee629cfb1b64111ffea
DARWIN\Usuario <u201816660@upc.edu.pe>
2018-09-29 01:08:53 -0500

    Cambios en usuarios

--- Parkeate_DW/db/schema.rb
+++ Parkeate_DW/db/schema.rb
@@ -1,4 +1,3 @@
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,10 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180929035014) do
 
-  create_table "Parkings", force: :cascade do |t|
+  create_table "parkings", force: :cascade do |t|
     t.integer "id_usuario"
     t.integer "id_ubicacion"
-    t.string "id_imagen"
     t.string "nombre"
     t.string "direccion"
     t.decimal "precio"
@@ -24,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180929035014) do
     t.integer "id_tipo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "id_imagen"
   end
 
   create_table "rents", force: :cascade do |t|
